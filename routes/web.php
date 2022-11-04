b1a978de5f68a3c3661d85750a80a6ae710ab42d
Douglas Kodi <kodi.shimoura@gmail.com>
2022-11-03 23:14:12 -0400

    procfile

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/link',function(){
+    Artisan::call('storage:link');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
